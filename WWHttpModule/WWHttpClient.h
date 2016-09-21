9d0b597e8363b3f6ce609a1a7945a662813ea5e4
wuxiaofang <wuxiaofang@yongche.com>
2016-09-21 15:25:57 +0800

    commit

---
+++ WWHttpModule/WWHttpClient.h
@@ -0,0 +1,15 @@
+//
+//  WWHttpClient.h
+//  WWHttpModule
+//
+//  Created by yongche_w on 16/9/21.
+//  Copyright © 2016年 ww. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WWHttpClient : NSObject
+
+- (void)printfClassInfo;
+
+@end
---
+++ WWHttpModule/WWHttpClient.m
@@ -0,0 +1,19 @@
+//
+//  WWHttpClient.m
+//  WWHttpModule
+//
+//  Created by yongche_w on 16/9/21.
+//  Copyright © 2016年 ww. All rights reserved.
+//
+
+#import "WWHttpClient.h"
+
+@implementation WWHttpClient
+
+- (void)printfClassInfo
+{
+    NSLog(@"ssssssss");
+    
+}
+
+@end
