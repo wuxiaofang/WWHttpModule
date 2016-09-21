4012495bb51c2c2ee9704be3647e03b7b09d3796
wuxiaofang <wuxiaofang@yongche.com>
2016-09-21 14:59:45 +0800

    Initial Commit

---
+++ WWHttpModule/WWHttpModule.h
@@ -0,0 +1,19 @@
+//
+//  WWHttpModule.h
+//  WWHttpModule
+//
+//  Created by yongche_w on 16/9/21.
+//  Copyright © 2016年 ww. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WWHttpModule.
+FOUNDATION_EXPORT double WWHttpModuleVersionNumber;
+
+//! Project version string for WWHttpModule.
+FOUNDATION_EXPORT const unsigned char WWHttpModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WWHttpModule/PublicHeader.h>
+
+
